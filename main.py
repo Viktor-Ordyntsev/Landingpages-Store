ee77f0b39ea7d53bc3f765cb4df82442a92aaa98
Viktor Ordynzev <disc.adm@yandex.ru>
2023-02-06 15:56:50 +0300

    Upload to GitLab(Dev)

--- main.py
+++ main.py
@@ -8,48 +8,48 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
-    # Главная страница
+    # Main page
     return render_template('index.html')
 
 
 @app.route("/success", methods=['POST'])
 def upload():
     if request.method == 'POST':
-        # Получение данных из HTML-формы
+        # Getting data from an HTML form
         file = request.files['landing_zipped_file']
         domain_name = request.form['domain_name']
         name_file = file.filename.split(".")
         
-        if (lm.Checking_domain_foк_Cyrillic(domain_name)): # Проверяем домен на кирилицу
+        if (lm.Checking_domain_for_Cyrillic(domain_name)):
             domain_name = lm.Cyrillic_to_Punycode_conversion(domain_name)
 
         if (lm.Domain_check(domain_name)):
-            if (name_file[1] == "zip"):  # Проверяем расширение полученного файла и домен
-                file.save(f"dist/{file.filename}")
-                lm.Unzipping(file.filename)  # Разархивирование
-
-                # Проверка на существование файла индексации
+            if (name_file[1] == "zip"):  # We check the extension of the received file and the domain
+                file.save(f'./{file.filename}')
+                lm.Unzipping(file.filename)
+                lm.Removing_service_directory(file.filename)
+                # Checking for the existence of an index file
                 if lm.Finding_and_changing_index_file(file.filename):
-                    flash('пока Все норм', category='info')
+                    if (lm.upoload_to_gitlab(name_file[0], domain_name)):
+                        if (lm.delet_local_directory(name_file[0])):
+                            flash('File uploaded successfully!', category='info')
+                            return redirect(url_for('main_page'))
+                        else:
+                            flash('Error: Unable to delete local directory', category='error')
+                            return redirect(url_for('main_page'))
+                    else:
+                        flash('Error: Failed to upload to remote repository', category='error')
+                        return redirect(url_for('main_page'))
+                else:
+                    flash('Error: Index file not found! Check if the file has the format "index.html" or "pageXXXXXXX.html"', category='error')
                     return redirect(url_for('main_page'))
-                    # if (lm.upoload_to_gitlab(name_file[0])):
-                    #     if (lm.delet_local_directory(name_file[0])):
-                    #         flash('Файл успешно загружен!', category='info')
-                    #         return redirect(url_for('main_page'))
-                    #     else:
-                    #         flash('Ошибка: Невозможно удалить локальную директорию', category='error')
-                    #         return redirect(url_for('main_page'))
-                    # else:
-                    #     flash('Ошибка: Не удалось загрузить в удаленный репозиторий', category='error')
-                    #     return redirect(url_for('main_page'))
-
             else:
-                flash('Ошибка: Не верный формат файла', category='error')
+                flash('Error: Invalid file format! Must be a zip archive', category='error')
                 return redirect(url_for('main_page'))
         else:
-            flash('Ошибка: Не верный домен', category='error')
+            flash('Error: Invalid domain! Perhaps the DNS record was created incorrectly', category='error')
             return redirect(url_for('main_page'))
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
--- moduls/logic_modul.py
+++ moduls/logic_modul.py
@@ -2,70 +2,67 @@ import os
 import shutil
 import zipfile
 import socket
-import subprocess
 
-global landing_dir
-landing_dir = "dist"
-#test
+
+
 
 def Unzipping(zip_filename: str) -> bool:
-    """ Функция для разархивирование zip-файлов. Получает на вход название файла, 
-    в результате создается директория с таким же названием, Функция возращает булевое значение."""
+    """ Function for unzipping zip files. Gets the name of the file as input
+     as a result, a directory with the same name is created. The function returns a boolean value."""
     try:
-        # Распаковка zip-файла
-        zf = zipfile.ZipFile(f"{landing_dir}/{zip_filename}")
-        zf.extractall(landing_dir)
+        # Unzip the zip file
+        zf = zipfile.ZipFile(f"./{zip_filename}")
+        zf.extractall('')
 
-        # Удаление сервисной директории???????
-        shutil.rmtree(os.path.join(os.path.abspath(os.path.dirname(
-            __file__)), f'{landing_dir}/__MACOSX'))
-        
-        os.remove(zip_filename)
 
-        # Исправление название дириктори
-        Files = os.listdir(f"{landing_dir}/")
+        # Correction of the name of the directory
+        Files = os.listdir('')
         for file in Files:
             if (file != zip_filename):
                 if (zip_filename.split(".")[0] in file):
-                    os.rename(f"{landing_dir}/{file}",
-                              f"{landing_dir}/{zip_filename.split('.')[0]}")
+                    os.rename(f"./{file}",f"./{zip_filename.split('.')[0]}")
                     return True
     except:
         return False
+def Removing_service_directory(zip_filename: str):
+    # Removing the service directory???????
+    shutil.rmtree(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../__MACOSX/'))
+    os.remove(f'/Users/a1/Downloads/Web/landing_py/landingpages-store/{zip_filename}')
+    print("removing")
 
 
 def Finding_and_changing_index_file(zip_filename: str) -> bool:
-    """ Функция поиска и измение файла индексации. На вход получает название zip-файла. 
-    В результате, файл индексации именуется 'index'. Функия возвращяет булевое значение. """
+    """ Search function and change the index file. It receives the name of the zip file as input.
+     As a result, the index file is named 'index'. The function returns a boolean value."""
     try:
-        # Получаем путь к рабочей директории
-        directory = f"{landing_dir}/{zip_filename.split('.')[0]}"
+        # Get the path to the working directory
+        directory = f"./{zip_filename.split('.')[0]}"
 
-        # Список всех файлов    
+        # List of all files   
         Files = os.listdir(directory)
 
-        # Переименовываем файл индексации
+        # Renaming the index file
         for file in Files:
             file_sp = file.split(".")
             if (file_sp[0] == "index"):
                 return True
             if ("page" in file_sp[0]):
                 os.rename(
-                    f"{landing_dir}/{zip_filename.split('.')[0]}/{file}", f"{landing_dir}/{zip_filename.split('.')[0]}/index.html")
+                    f"./{zip_filename.split('.')[0]}/{file}", f"./{zip_filename.split('.')[0]}/index.html")
                 return True
     except:
         return False
 
 
 def Domain_check(domain: str) -> bool:
-    """Функция для проверки привязанности доменна к серверу. 
-    На вход подается строка - имя домена.
-    На выходе булевое значение"""
+    """A function for checking domain-to-server binding.
+     The input is a string - the name of the domain.
+     Output boolean value"""
     try:
-        # Резолвим домен, получай ipv4
+        # Resolve domain, get ipv4
         ip_v4_lst = socket.gethostbyname_ex(domain)
 
-        # Сравниваем с ip полученным из переменных окружения
+        # Compare with ip obtained from environment variables
         if (os.environ["IP_ADDRESS"] in ip_v4_lst[2]):
             return True
         else:
@@ -75,28 +72,47 @@ def Domain_check(domain: str) -> bool:
 
 
 def Cyrillic_to_Punycode_conversion(Cyrillic_domain: str) -> str:
-    """Функция конвертации Кирилического домена в Punycode"""
+    """Cyrillic domain to Punycode conversion function"""
     return Cyrillic_domain.encode('idna').decode()
 
 
-def Checking_domain_foк_Cyrillic(Cyrillic_domain: str) -> bool:
-    """Функция проверки на содержание Кирилиц в домене"""
+def Checking_domain_for_Cyrillic(Cyrillic_domain: str) -> bool:
+    """The function of checking for the content of Cyrillic in the domain"""
     alphabet = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
                 "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"}
 
     return bool(alphabet.intersection(set(Cyrillic_domain.lower())))
 
 
-def upoload_to_gitlab(poject_name: str) -> bool:
-    """Функция выгрузки локальной директории на удаленные репозиторий git """
-    pass
+def upoload_to_gitlab(poject_name: str, domain_name: str):
+    """The function of uploading a local directory to remote git repositories """
+    import git
+
+    repo_dir = 'repo/'
+    repo = git.Repo.clone_from('git@gitlab.e-queo.xyz:viktor_ordyntsev/landing-storage.git', repo_dir)
+    git = repo.git
+
+    new_branch = repo.create_head(domain_name, 'HEAD~2')
+    repo.head.reference = new_branch
+
+    shutil.move(f'{poject_name}/', 'repo/')
+    os.rename(f'repo/{poject_name}', 'repo/dist')
+
+    get_ls_file = [i for i in os.listdir('repo/') if i[0] != '.']
+
+    repo.index.add(get_ls_file)
+    repo.index.commit(domain_name)
+
+    git.push('-u','origin','--all')
+    return True
+
 
 def delet_local_directory(poject_name: str) -> bool:
-    """Функция удаления локальной директории"""
+    """Delete local directory function"""
     try:
-        path = os.path.join(os.path.abspath(
-        os.path.dirname(__file__)), f'{landing_dir}/{poject_name}')
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'./{poject_name}')
         shutil.rmtree(path)
+        shutil.rmtree(os.path.join(os.path.abspath(os.path.dirname(__file__)), './repo'))
         return True
     except:
         return False
